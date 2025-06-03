6c7f0a8f91d6820f189650deaed98fb3398acc3f
zhanguoxingzhe <zhanguoxingzhe@gmail.com>
2025-06-03 18:57:23 +0800

    remove popularCollection

--- src/app/(home)/rank/[id]/page.tsx
+++ src/app/(home)/rank/[id]/page.tsx
@@ -1,6 +1,6 @@
 import RankComments from "~/app/(home)/rank/[id]/_components/rank-comment"
 import RankDetail from "~/app/(home)/rank/[id]/_components/rank-detail"
-import PopularCollection from "~/app/(home)/rank/[id]/_components/popular-collection"
+// import popularCollection from "~/app/(home)/rank/[id]/_components/popular-collection"
 import { api } from "~/trpc/server"
 import RankContent from "./_components/rank-content"
 const RankerPage = async ({ params }: { params: Promise<{ id: string }> }) => {
@@ -12,7 +12,7 @@ const RankerPage = async ({ params }: { params: Promise<{ id: string }> }) => {
       {detail && <RankDetail detail={detail} id={id} />}
       <RankContent id={id} />
       <RankComments id={id} />
-      <PopularCollection />
+      {/* <CuratedCollection /> */}
     </>
   )
 }
